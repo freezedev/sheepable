08108304992d6cf51157e82d9c23cef075e7107d
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-10 01:57:06 +0100

    Map stub

--- src/map.js
+++ src/map.js
@@ -1,3 +1,21 @@
 var Map = function() {
 
 };
+
+Map.prototype.clone = function() {
+
+};
+
+Map.prototype.get = function(name) {
+
+};
+
+Map.prototype.set = function(name, value) {
+
+};
+
+Map.prototype.toJSON = function() {
+
+};
+
+module.exports = Map;
